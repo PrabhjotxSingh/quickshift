a847589922c89b354da452231854d655d4de6cff
Russell Toney <91279889+ru55e1l@users.noreply.github.com>
2025-02-10 18:32:57 -0500

    checkpoitn

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -20,62 +20,62 @@ const conOptions: ConnectOptions = { autoCreate: true, autoIndex: false };
 DebugUtil.log("Connecting to database");
 mongoose
 	.connect(process.env.DB_CONNECTION_STRING, conOptions)
-	.then(async () => {
-		// Second round of imports so that database models arente created before
-		// mongoose connection is established
-		const { RegisterRoutes } = await import("./routes");
-
-		DebugUtil.log("Registering parsers");
-		app.use(bodyParser.urlencoded({ extended: true }));
-		app.use(bodyParser.json());
-		app.use(express.json());
-		app.use(cookieParser(process.env.SECRET));
-
-		// middleware
-		DebugUtil.log("Registering context middleware");
-		app.use(contextMiddleware);
-
-		// for debugging the auth
-		// probably not needed anymore
-		if (true) {
-			app.use((req, res, next) => {
-				console.log("Request cookies:", req.cookies);
-				console.log("Request signed cookies:", req.signedCookies);
-				next();
-			});
-		}
-
-		DebugUtil.log("Registering cors");
-		app.use(
-			cors({
-				origin: "*",
-				credentials: true,
-				exposedHeaders: ["set-cookie"],
-				methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
-				allowedHeaders: ["Content-Type", "Authorization", "Cookie", "Set-Cookie"],
-			}),
-		);
-
-		DebugUtil.log("Registering routes");
-		RegisterRoutes(app);
-
-		DebugUtil.log("Registering swagger");
-		app.use(
-			"/",
-			swaggerUi.serve,
-			swaggerUi.setup(swaggerDocument, {
-				swaggerOptions: {
-					withCredentials: true,
-					persistAuthorization: true,
-					requestInterceptor: (req: any) => {
-						req.credentials = "include";
-						return req;
-					},
-				},
-			}),
-		);
-	})
+	.then(async () => {})
 	.catch((err) => {
 		DebugUtil.error(new Error("Error occured connecting to database: " + err));
 	});
+
+// Importing after the mongoose.connect() as the models
+// need to be registered after the connection is established
+import { RegisterRoutes } from "./routes";
+
+DebugUtil.log("Registering parsers");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cookieParser(process.env.SECRET));
+
+// middleware
+DebugUtil.log("Registering context middleware");
+app.use(contextMiddleware);
+
+// for debugging
+// probably not needed anymore
+if (false) {
+	app.use((req, res, next) => {
+		console.log("Request cookies:", req.cookies);
+		console.log("Request signed cookies:", req.signedCookies);
+		next();
+	});
+}
+
+DebugUtil.log("Registering cors");
+app.use(
+	cors({
+		origin: "*",
+		credentials: true,
+		exposedHeaders: ["set-cookie"],
+		methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+		allowedHeaders: ["Content-Type", "Authorization", "Cookie", "Set-Cookie"],
+	}),
+);
+
+DebugUtil.log("Registering routes");
+RegisterRoutes(app);
+
+DebugUtil.log("Registering swagger");
+app.use(
+	"/docs",
+	swaggerUi.serve,
+	swaggerUi.setup(swaggerDocument, {
+		swaggerOptions: {
+			withCredentials: true,
+			persistAuthorization: true,
+			requestInterceptor: (req: any) => {
+				req.credentials = "include";
+				return req;
+			},
+		},
+	}),
+);
 export { app };
