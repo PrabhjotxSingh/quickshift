48e342a470feef6453d23e0bd12bbbd26b4415f6
Russell Toney <91279889+ru55e1l@users.noreply.github.com>
2025-04-06 22:07:06 -0400

    Update user-dto.profile.ts

--- backend/src/core/utility/mapper/user/user-dto.profile.ts
+++ backend/src/core/utility/mapper/user/user-dto.profile.ts
@@ -4,6 +4,7 @@ import { UserModel, UserDocument } from "../../../model/user.model";
 import { UserDto } from "../../../dto/models/user.dto";
 
 export const userDtoProfile: MappingProfile = (mapper) => {
+	// Map from UserModel to UserDto
 	createMap(
 		mapper,
 		UserModel,
@@ -39,6 +40,41 @@ export const userDtoProfile: MappingProfile = (mapper) => {
 		forMember(
 			(destination) => destination.skills,
 			mapFrom((source: UserDocument) => source.skills),
+		),
+	);
+
+	// Map from UserDto to UserModel
+	createMap(
+		mapper,
+		UserDto,
+		UserModel,
+		forMember(
+			(destination) => destination.email,
+			mapFrom((source) => source.email),
+		),
+		forMember(
+			(destination) => destination.username,
+			mapFrom((source) => source.username),
+		),
+		forMember(
+			(destination) => destination.password,
+			mapFrom((source) => source.password),
+		),
+		forMember(
+			(destination) => destination.firstName,
+			mapFrom((source) => source.firstName),
+		),
+		forMember(
+			(destination) => destination.lastName,
+			mapFrom((source) => source.lastName),
+		),
+		forMember(
+			(destination) => destination.roles,
+			mapFrom((source) => source.roles),
+		),
+		forMember(
+			(destination) => destination.skills,
+			mapFrom((source) => source.skills),
 		),
 	);
 };
