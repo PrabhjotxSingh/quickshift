353ea715500c604dedb177b27a47f09ea4cd649f
Russell Toney <91279889+ru55e1l@users.noreply.github.com>
2025-04-07 19:45:59 -0400

    c

--- backend/src/core/repository/shift-applicant.repository.ts
+++ backend/src/core/repository/shift-applicant.repository.ts
@@ -3,10 +3,15 @@ import { UserDocument } from "../model/user.model";
 import { Repository } from "./base.repository";
 import { ShiftDocument, ShiftModel } from "../model/shift.model";
 import { ShiftApplicantDocument, ShiftApplicantModel } from "../model/shift-applicant.model";
+import { FilterQuery } from "mongoose";
 
 @Service()
 export class ShiftApplicantRepository extends Repository<ShiftApplicantDocument> {
 	constructor() {
 		super(ShiftApplicantModel);
+	}
+
+	async getManyByQuery(query: FilterQuery<ShiftApplicantDocument>): Promise<ShiftApplicantDocument[]> {
+		return super.getManyByQuery(query) as Promise<ShiftApplicantDocument[]>;
 	}
 }
